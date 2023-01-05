19da793628e2daf068e39c9e7045353f6e18a0d8
Dereck Rascon <dereckrascon76@gmail.com>
2023-01-04 19:45:19 -0700

    Finished Bob exercise

---
+++ src/Bob.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+            System.out.println("What would you like to tell Bob??");
+            String input = scanner.nextLine();
+            if (input.endsWith("?")) {
+                System.out.println("Bob response: Sure.");
+            } else if (input.endsWith("!")) {
+                System.out.println("Bob response: Whoa, chill out!!");
+            } else if (input.isEmpty()) {
+                System.out.println("Bob response: Fine. Be that way!");
+            } else {
+                System.out.println("Bob response: Whatever!");
+            }
+
+    }
+}
