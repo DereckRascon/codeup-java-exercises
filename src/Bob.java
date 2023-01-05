ffac91f7b65681c6c204e240a6ad1b69d13dcd6a
Dereck Rascon <dereckrascon76@gmail.com>
2023-01-05 09:59:21 -0700

    updated Bob exercise

--- src/Bob.java
+++ src/Bob.java
@@ -3,19 +3,21 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+//
+//            System.out.println("What would you like to tell Bob??");
+//            String input = scanner.nextLine();
+//            if (input.endsWith("?")) {
+//                System.out.println("Bob response: Sure.");
+//            } else if (input.endsWith("!")) {
+//                System.out.println("Bob response: Whoa, chill out!!");
+//            } else if (input.isEmpty()) {
+//                System.out.println("Bob response: Fine. Be that way!");
+//            } else {
+//                System.out.println("Bob response: Whatever!");
+//            }
 
-            System.out.println("What would you like to tell Bob??");
-            String input = scanner.nextLine();
-            if (input.endsWith("?")) {
-                System.out.println("Bob response: Sure.");
-            } else if (input.endsWith("!")) {
-                System.out.println("Bob response: Whoa, chill out!!");
-            } else if (input.isEmpty()) {
-                System.out.println("Bob response: Fine. Be that way!");
-            } else {
-                System.out.println("Bob response: Whatever!");
-            }
+//        Instructor way
 
     }
 }
