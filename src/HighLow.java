5bba73b213503bff8e0c4c40f5ee629fb1c7ff0c
Dereck Rascon <dereckrascon76@gmail.com>
2023-01-09 21:14:34 -0700

    Finished HighLow exercise

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,22 +1,48 @@
 import java.util.Scanner;
-//
-//public class HighLow {
-//    public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
-//         int numberRange = 100 - 1 + 1;
-//         int luckyNumber = (int) ((Math.random() * range) + 1);
-//         System.out.println("luckyNumber = " + luckyNumber);
-//         game(numberRange, scanner);
-//
-//    }
-//
-//    public static void game(int numberRange, Scanner scanner){
-//        while(true){
-//            System.out.println("Take a younder at a number between 1 and 100");
-//            short userInput = Short.parseShort(scanner.nextLine());
-//            if(userInput > luckyNumber){
-//                System.out.println("LOWER");
-//            }
-//        }
-//    }
-//}
+import java.lang.Math;
+
+public class HighLow {
+
+    public static int randomNumber(){
+        return (int)(Math.random()* 100)+1;
+    }
+    public static int guessConfirm(){
+        int min = 1;
+        int max = 100;
+        System.out.println("Take a younder of a number between 1 and 100!");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        if (input > max || input <min){
+            System.out.printf("Sorry that is not a valid number! Please enter a number between %s to %s:", min, max);
+            System.out.println();
+            return guessConfirm();
+        }
+        System.out.printf("You entered %s!", input);
+        return input;
+    }
+
+    public static int guessNumber(int randomNum){
+        System.out.println(randomNum);
+        int number = 0;
+        System.out.println();
+        while(number != randomNum){
+            number = guessConfirm();
+            System.out.println();
+            if(number > randomNum){
+                System.out.println("LOWER");
+            }
+            else if(number < randomNum){
+                System.out.println("HIGHER");
+            }
+        }
+        if (number == randomNum){
+            System.out.print("Bingo! you got it!!");
+        }
+        return number;
+    }
+
+    public static void main(String[] args){
+        guessNumber(randomNumber());
+    }
+
+}
